4ec5eb4ae44c1eb19b2973b679f9962a7afe5226
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2023-12-08 21:07:25 -0500

    Initial Full Dev

---
+++ react-todo-list/src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaPen, FaTrash  } from 'react-icons/fa'
+export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+ 
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+
+        <div>
+          <FaPen style={{marginRight: "8px"}} onClick={() => editTodo(task.id)} />
+          <FaTrash onClick={() => deleteTodo(task.id)} />
+        </div>
+    </div>
+  )
+}
